166353fec1bb7c8a426284631993bf5699433525
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2022-03-17 14:10:59 +0330

    complete about page and its responsive

--- page-template/about.php
+++ page-template/about.php
@@ -6,26 +6,31 @@ get_template_part( '/partials/head-pages' );
 ?>
 	</header>
 <?php $contact_info = mhdr_get_option('mhd_contact_setting') ?>
-	<div class="main-about">
-		<div class="col-md-12 col-sm-12 main-about-page">
-			<div class="container">
+	<div class="about">
+		<div class="col-md-12 col-sm-12 ">
+			<div class="about__main">
+				<div class="container">
 				<?php if (have_posts()){
 				while (have_posts()) {
 				the_post();
 				$id_post = get_the_ID();
 				mhd_update_post_view( $id_post );
 				?>
-				<div class="about-us">
-					<div class=" col-md-12 about-capability">
-						<div class="about-content" >
+					<div class=" col-md-12 col-12">
+						<div class="about__content" >
 							<?php the_content(); ?>
 						</div>
-						<div class="col-md-12 about-content">
-						<div class="col-md-6 col-sm-12 about-img">
-							<img src="<?php echo get_template_directory_uri().'/images/double-like.png' ?>" class="img-responsive" alt="">
-
-					</div>
-						<div class="col-md-6 col-sm-12 about-cap"> <?php
+						
+							<div class="about__capability" >
+								<div class="row align-items-center" >					
+									<div class="col-md-6 col-sm-12 ">
+										<div class="about__img">
+										<img src="<?php echo get_template_directory_uri().'/images/double-like.png' ?>" class="img-responsive" alt="محمد رضایی">
+									</div>
+								</div>
+						<div class="col-md-6 col-sm-12 ">
+							<div class="about__cap">
+							<?php
 							$capability_infos = mhdr_get_option('mhd_capability_setting');
 							foreach ($capability_infos as $capability_info){
 								?>
@@ -34,10 +39,12 @@ get_template_part( '/partials/head-pages' );
 										<span><?php echo $capability_info['mhd_capability_title'];  ?></span>
 									</div>
 								</div>
-							<?php } ?></div>
+							<?php } ?>
 						</div>
 					</div>
-					</div>
+				</div>
+			</div>
+	</div>
 				</div>
 				<?php
 				}
@@ -45,6 +52,7 @@ get_template_part( '/partials/head-pages' );
 				?>
 			</div>
 		</div>
+	</div>
 
 
 
